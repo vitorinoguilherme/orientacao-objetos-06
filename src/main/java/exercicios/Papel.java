90b1927d83c881d5774d4491d072adc9ebed403c
Guilherme Vitorino <guilherme.vitorino@indraenergia.com>
2020-08-30 14:36:16 -0300

    Implementado Classe Papel

---
+++ src/main/java/exercicios/Papel.java
@@ -0,0 +1,38 @@
+package exercicios;
+
+public class Papel {
+    private String nome;
+    private boolean protagonista;
+    private Ator ator;
+    private Filme filme;
+
+    public Papel(String nome, boolean protagonista, Ator ator, Filme filme) {
+        this.setNome(nome);
+        this.protagonista = protagonista;
+        this.ator = ator;
+        this.filme = filme;
+    }
+
+    public void setNome(String nome) {
+        if ( nome == null ) {
+            throw new IllegalArgumentException("O nome não pode ser nulo. ");
+        }
+        this.nome = nome;
+    }
+
+    public void setProtagonista(boolean protagonista) {
+        this.protagonista = protagonista;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Ator getAtor() {
+        return ator;
+    }
+
+    public Filme getFilme() {
+        return filme;
+    }
+}
