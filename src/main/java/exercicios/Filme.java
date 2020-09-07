7d1937dde9f6680f6a1c1668557c06443108b6c9
Guilherme Vitorino <guilherme.vitorino@indraenergia.com>
2020-09-06 21:42:10 -0300

    Implementado métodos addPapel e getProtagonista a classe Filme

--- src/main/java/exercicios/Filme.java
+++ src/main/java/exercicios/Filme.java
@@ -14,16 +14,15 @@ public class Filme {
         this.papeis = new ArrayList<>();
     }
 
-    public void addPapel(Ator ator, String nome, boolean protagonista) {
-        //Papel papel = new Papel(nome, true);
-        //this.papeis.add();
-    }
-
     public void setTitulo(String titulo) {
         if ( titulo == null ) {
             throw new IllegalArgumentException("O título não pode ser nulo. ");
         }
         this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
     }
 
     public void setAno(int ano) {
@@ -33,12 +32,25 @@ public class Filme {
         this.ano = ano;
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
     public int getAno() {
         return ano;
+    }
+
+    public void addPapel(Ator ator, String nome, boolean protagonista) {
+        if(protagonista) {
+            for (Papel p: papeis) {
+                if (p.isProtagonista()) {
+                    throw new IllegalArgumentException("O filme já tem um protagonista.");
+                }
+
+            }
+        }
+        Papel papel = new Papel();
+        papel.setAtor(ator);
+        papel.setNome(nome);
+        papel.setProtagonista(protagonista);
+
+        this.papeis.add(papel);
     }
 
     public List<Papel> getPapeis() {
@@ -55,9 +67,13 @@ public class Filme {
         return listAtor;
     }
 
-//    public Ator getProtagonista() {
-//        for (Papel p : this.getPapeis()) {
-//
-//        }
-//    }
+    public Ator getProtagonista() {
+        for (Papel p : this.getPapeis()) {
+            if (p.isProtagonista()) {
+                return p.getAtor();
+            }
+        }
+        return null;
+    }
+
 }
